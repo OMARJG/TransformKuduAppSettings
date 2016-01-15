039a4407e50c775b00d1121bdd53dc379b3e3a56
puneet-gupta <puneetg@microsoft.com>
2016-01-15 16:19:07 +0530

    throwing exception

--- TransformKuduAppSettings/WebForm1.aspx.cs
+++ TransformKuduAppSettings/WebForm1.aspx.cs
@@ -11,7 +11,7 @@ namespace TransformKuduAppSettings
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            throw new ApplicationException("something");
         }
     }
 }
